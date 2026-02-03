35e1960397907cd1b78edea62f92d4e7b6d51b2a
Seu Nome <lucasfermagalhaes@gmail.com>
2026-02-03 16:01:26 -0300

    feat: adicionando tratamentos de erros com classes externas.

--- src/main/java/med/vol/api/Infra/TratadorDeErros.java
+++ src/main/java/med/vol/api/Infra/TratadorDeErros.java
@@ -1,4 +1,39 @@
 package med.vol.api.Infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.LinkedHashMap;
+
+@RestControllerAdvice
 public class TratadorDeErros {
+
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratamento404(){
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratamento400(MethodArgumentNotValidException ex){
+        var errors = ex.getFieldErrors();
+
+        //converte a lista do getFieldErrors para uma lista do dto
+        return ResponseEntity.badRequest().body(errors.stream().map(DadosErrosValidacao::new).toList());
+    }
+
+    private record DadosErrosValidacao(String campo, String mensagem){
+        public DadosErrosValidacao(FieldError fieldError){
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+    }
+
+
+
 }
