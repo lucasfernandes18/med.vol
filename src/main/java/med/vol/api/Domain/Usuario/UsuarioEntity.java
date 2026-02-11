a8549729d510ddb2423e7c048b80ea8a0b243f08
Seu Nome <lucasfermagalhaes@gmail.com>
2026-02-11 15:29:11 -0300

    feat: adicionado a interface UserDetails, implementando os seus métodos e indicando que senha é passada como hash.

--- src/main/java/med/vol/api/Domain/Usuario/UsuarioEntity.java
+++ src/main/java/med/vol/api/Domain/Usuario/UsuarioEntity.java
@@ -5,6 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuario")
@@ -12,7 +20,7 @@ import lombok.ToString;
 @NoArgsConstructor
 @Data
 @ToString
-public class UsuarioEntity {
+public class UsuarioEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,4 +28,39 @@ public class UsuarioEntity {
     private String login;
     private String senha;
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USUARIO"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
